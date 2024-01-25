af5439e822eaef541d7231a84ea17061fe17eb08
AleSassi <AleSassi@users.noreply.github.com>
2024-01-25 10:40:37 +0000

    Deleted Django files

--- backend/django/backend_api/models/collection.py
+++
@@ -1,9 +0,0 @@
-from django.core.files.storage import FileSystemStorage
-from django.db import models
-from django import forms
-
-fs = FileSystemStorage(location="media/files/")
-
-class CollectionModel(models.Model):
-    title = forms.CharField(max_length=50)
-    file = forms.FileField(storage=fs)
